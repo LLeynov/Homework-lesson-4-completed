2764290bc45d30de137cfa132f4210561d94cdb9
Lev <lleynov2018@mail.ru>
2022-04-02 20:28:29 +0300

    Выполнен второй пункт домашнего задания.

--- Homework lesson 4/Program.cs
+++ Homework lesson 4/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework_lesson_4
+namespace ConsoleApp
 {
-    internal class Program
+    class Program
     {
-        /* Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и
-        возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести
-        результат на экран. */
+
+        // Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и
+        //возвращающую число — сумму всех чисел в строке.Ввести данные с клавиатуры и вывести
+        //результат на экран.
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите желаемые числа через пробел. ");
+            var sum = Console.ReadLine().Split().Select(int.Parse).Sum();
+            Console.WriteLine("Сумма введённых чисел равна : " + sum);
         }
-    }
+
+
+    }      
+    
 }

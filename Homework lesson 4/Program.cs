b20a9c7f66cc8c1669c9d7842046a4d53be283b8
Lev <lleynov2018@mail.ru>
2022-04-04 21:41:52 +0300

    Выполнен четвёртый пункт домашнего задания.

--- Homework lesson 4/Program.cs
+++ Homework lesson 4/Program.cs
@@ -7,12 +7,28 @@ using System.Threading.Tasks;
 namespace Homework_lesson_4
 {
     //Написать программу, вычисляющую число Фибоначчи для заданного значения
-    //рекурсивным способом.
+    //рекурсивным способом. Предыдущее число + Предпредыдущее число = Текущее число.
 
     internal class Program
     {
 
-    }
 
-    
+        static int Fibonacci(int i )
+        {
+            if ( i == 0 || i == 1 ) return i;
+            int x = Fibonacci(i - 1) + Fibonacci(i - 2);
+            Console.WriteLine(x);
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("До какого числа выводить значения Фибоначчи? ");
+            int i = int.Parse(Console.ReadLine());
+            if (i <= 15)
+                Fibonacci(i);
+            else
+                Console.WriteLine("Вы ввели слишком большое число,введите число поменьше. ");
+        }
+    }
 }
